765598c61e31f8762fd2f80cb989e5bc24b8fc27
Arkadip Ghosh <110709120+Creating-Content@users.noreply.github.com>
2025-06-20 00:50:04 +0530

    Update Crop.py

--- Crop.py
+++ Crop.py
@@ -168,7 +168,7 @@ st.markdown(f"""
         box-shadow: 0 8px 30px rgba(0, 0, 0, 0.2);
         border: 1px solid #d4edda;
         margin: 120px auto 30px auto;
-        max-width: 50%;
+        max-width: 45%;
     }}
 
     /* Header Title */
